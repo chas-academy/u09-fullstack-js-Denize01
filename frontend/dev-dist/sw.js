e27e9ce4f067f5cfe2dc57390ed440a707b550f2
Denize <denize.yildirim@chasacademy.se>
2024-09-26 16:57:16 +0200

    trying pwa, not working

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.634h763okq"
+    "revision": "0.ks1avoiroeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
