bd2260087694349de12303f9611b84bf6949e974
Denize <denize.yildirim@chasacademy.se>
2024-10-07 12:01:50 +0200

    frontend dev dist folder

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v0a5b5kqt68"
+    "revision": "0.7kbpjt5bl4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
