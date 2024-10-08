c41cffac475d8e8121a71eead2fa66a557e386d4
Denize <denize.yildirim@chasacademy.se>
2024-10-08 09:54:56 +0200

    installed react router dom

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7kbpjt5bl4g"
+    "revision": "0.44d1jdh064"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
