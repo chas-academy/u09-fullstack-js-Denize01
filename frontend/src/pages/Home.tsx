e823dc6707e278ef2c8107ea5124eb170b0ee510
Denize <denize.yildirim@chasacademy.se>
2024-10-11 06:03:57 +0200

    installed latest tailwind, some minro styling to home.tsx, tried getting animation activated. still in progress, not done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,7 +7,8 @@ const Home: React.FC = () => {
       <div className="bg-white bg-opacity-20 border-4 border-gray-400 backdrop-blur-lg p-8 rounded-xl shadow-xl max-w-lg mx-auto">
         {/* Välkomsttext */}
         <h1 className="text-4xl font-bold mb-6 text-center">
-          Welcome to Trackster
+          Welcome to{" "}
+          <span className="text-pink-500 animate-bounceOnce">Trackster</span>
         </h1>
         <p className="text-lg text-gray-700 mb-8 text-center">
           ... your personal training calendar to help you keep track of your
