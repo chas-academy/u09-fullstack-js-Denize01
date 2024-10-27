c8add683dbe4247231427de9e2ddc86d912fd127
Denize <denize.yildirim@chasacademy.se>
2024-10-27 23:09:26 +0100

    readme

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -8,33 +8,36 @@ const express_session_1 = __importDefault(require("express-session"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const activityRoutes_1 = __importDefault(require("./routes/activityRoutes"));
 const db_1 = __importDefault(require("./config/db"));
-const cors_1 = __importDefault(require("cors")); // Importera CORS
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const app = (0, express_1.default)();
 dotenv_1.default.config();
-// const PORT = process.env.PORT || 3000;
-// const FRONTEND_URL = process.env.REACT_APP_API_BASE_URL || "https://tracksterapp.netlify.app/";
-// const SESSION_SECRET = process.env.SESSION_SECRET || "your-secret-key"; // Fallback för utveckling
-// Använd CORS som middleware
+const app = (0, express_1.default)();
+// Använd miljövariabler för frontend-URL och sessionshemlighet
+const FRONTEND_URL = process.env.REACT_APP_API_BASE_URL || "http://tracksterapp.netlify.app";
+const SESSION_SECRET = process.env.SESSION_SECRET || "your-secret-key";
+// Konfigurera CORS
 app.use((0, cors_1.default)({
-    origin: `https://tracksterapp.netlify.app/`, //http://localhost:5173
-    credentials: true,
+    origin: FRONTEND_URL, // Tillåt endast anrop från frontend-URL
+    credentials: true, // Tillåt cookies vid API-anrop
 }));
 app.use(express_1.default.json());
 // Konfigurera sessionshantering
 app.use((0, express_session_1.default)({
-    secret: "SESSION_SECET", // Borde lagras i .env-filen
+    secret: SESSION_SECRET, // Hämtas från .env
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true }, // Sätt till 'true' om du använder HTTPS
+    cookie: { secure: process.env.NODE_ENV === "production" }, // Säkra cookies endast i produktion
 }));
+// Anslut till databasen
 (0, db_1.default)();
+// Testroute
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
+// API-rutter
 app.use("/api", userRoutes_1.default);
 app.use("/api", activityRoutes_1.default);
-// I testsyfte
+// Testroute för att verifiera API-åtkomst
 app.get("/test", (req, res) => {
     res.json({ message: "Test route works!" });
 });
